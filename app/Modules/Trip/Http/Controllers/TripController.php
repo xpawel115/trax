e02ab54b914132cf2e0097ad588463db3db5e1aa
Pawel Raś <xpawel115@gmail.com>
2021-09-10 10:51:39 +0200

    refactor: rename Trip Controller

--- app/Modules/Trip/Http/Controllers/TripController.php
+++ app/Modules/Trip/Http/Controllers/TripController.php
@@ -5,7 +5,7 @@ namespace App\Modules\Trip\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class TripHttpControllersTripController extends Controller
+class TripController extends Controller
 {
     //
 }
